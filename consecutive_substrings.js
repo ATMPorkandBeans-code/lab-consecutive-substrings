658ae805802ff9b258ccd170a21f4c0504b74d64
Adrian Moring <adriantmoringbass@gmail.com>
2025-09-16 08:15:56 -0400

    first algorithm iteration

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,5 +1,12 @@
 function consecutiveSubstrings(string) {
   // type your code here
+  const subStrings = [];
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i; j < string.length; j++){
+      subStrings.push(string.slice(i, j + 1));
+    }
+  }
+  return subStrings;
 }
 
 if (require.main === module) {
